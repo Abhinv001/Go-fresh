c74f088eb7dc17a86ab5e8df34bd04258f11932d
Abhinv001 <asinghofficial01@gmail.com>
2024-04-26 11:42:05 +0530

    login page

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,7 @@
+
+<footer class="footer mt-auto py-3 bg-light">
+    <div class="container text-center">
+        <span class="text-muted">&copy; 2024 Your Company Name. All rights reserved.</span>
+    </div>
+</footer>
+
